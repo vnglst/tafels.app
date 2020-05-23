96948750086f8939ba6ed830bc29943285ca06ab
Koen van Gilst <koen.vangilst@gmail.com>
2020-05-23 10:01:59 +0200

    fix() - icons

--- src/ui/IconUserCircle.svelte
+++ src/ui/IconUserCircle.svelte
@@ -1,3 +1,23 @@
+<svg
+  height="28"
+  width="28"
+  viewBox="0 0 24 24"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    class="primary"
+    fill="currentColor"
+    d="m3.66 17.52a10 10 0 1 1 16.68 0c-.86-1.5-2.48-1.52-4.34-1.52h-8c-1.86
+    0-3.48.01-4.34 1.52z"
+  />
+  <path
+    class="secondary"
+    fill="currentColor"
+    d="m3.66 17.52a5 5 0 0 1 4.34-2.52h8a5 5 0 0 1 4.34 2.52 10 10 0 0 1 -16.68
+    0zm8.34-4.52a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"
+  />
+</svg>
+
 <style>
   .primary {
     color: var(--white);
@@ -7,20 +27,3 @@
     color: var(--grey-800);
   }
 </style>
-
-<svg
-  height="28"
-  width="28"
-  viewBox="0 0 24 24"
-  xmlns="http://www.w3.org/2000/svg">
-  <path
-    class="primary"
-    fill="currentColor"
-    d="m3.66 17.52a10 10 0 1 1 16.68 0c-.86-1.5-2.48-1.52-4.34-1.52h-8c-1.86
-    0-3.48.01-4.34 1.52z" />
-  <path
-    class="secondary"
-    fill="currentColor"
-    d="m3.66 17.52a5 5 0 0 1 4.34-2.52h8a5 5 0 0 1 4.34 2.52 10 10 0 0 1 -16.68
-    0zm8.34-4.52a4 4 0 1 1 0-8 4 4 0 0 1 0 8z" />
-</svg>
